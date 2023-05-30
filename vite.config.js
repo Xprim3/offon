b7d96409cfcc1b17b596a10d4799a015efec4cdb
Xprim3 <129306389+Xprim3@users.noreply.github.com>
2023-05-30 19:55:09 +0200

    secound commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        profile: resolve(__dirname, 'profile.html'),
+        admin: resolve(__dirname, 'admin.html'),
       },
     },
   },
